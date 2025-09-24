4b2ea22efaa9a01e29a743231c6992b55f5de5ff
[BE9]도석환 <do04080@gmail.com>
2025-09-24 15:22:37 +0900

    feat: 상품 추가 기능 추가

--- Frontend/my-app/src/lib/client.ts
+++ Frontend/my-app/src/lib/client.ts
@@ -1,5 +1,4 @@
-import {ItemDto} from "@/type/items";
-
+import { ItemDto, ItemCreateRequest } from "@/type/items";
 export function fetchApi(url: string, options?: RequestInit) {
     if (options?.body) {
         const headers = new Headers(options.headers || {});
@@ -7,7 +6,7 @@ export function fetchApi(url: string, options?: RequestInit) {
         options.headers = headers;
     }
 
-    return fetch(`api/v1${url}`, options).then(
+    return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options).then(
         async (res) => {
             if (!res.ok) {
                 const rsData = await res.json();
@@ -25,17 +24,21 @@ export function getItems(): ItemDto[]  {
             itemId: 1,
             itemName: "name1",
             price: 100,
-            createdDate: "2025-09-24",
-            status: "sale",
             imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.given-log.com%2Fpost%2Fdev%2F20241015-react-useState&psig=AOvVaw1_0Spntces3qGBsDH_U5Ml&ust=1758762393208000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPi56tGa8I8DFQAAAAAdAAAAABAE"
         },{
             itemId: 2,
             itemName: "name2",
             price: 200,
-            createdDate: "2025-09-25",
-            status: "sale",
             imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.given-log.com%2Fpost%2Fdev%2F20241015-react-useState&psig=AOvVaw1_0Spntces3qGBsDH_U5Ml&ust=1758762393208000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPi56tGa8I8DFQAAAAAdAAAAABAE"
         }];
 
 
+}
+
+//상품 추가
+export function createItem(itemData: ItemCreateRequest): Promise<ItemDto> {
+    return fetchApi('/items', {
+        method: 'POST',
+        body: JSON.stringify(itemData),
+    });
 }
