b3c42c291e7de542a49060425d58e6cff1d20c15
[BE9]도석환 <do04080@gmail.com>
2025-09-24 17:25:54 +0900

    feat: WebMvcConfig 추가

--- Backend/src/main/java/com/example/cafe/global/webMvcConfig/WebMvcConfig.java
+++ Backend/src/main/java/com/example/cafe/global/webMvcConfig/WebMvcConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
@@ -16,4 +15,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
